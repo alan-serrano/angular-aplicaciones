5d6601d6f07beeb2d4189db9dd419f7e16fcf245
alan-serrano <alan.serrano.pch@gmail.com>
2019-09-18 11:56:11 -0400

    Clases en TypeScript

---
+++ Ejercicios/21.-Clases_y_POO/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var Avenger = /** @class */ (function () {
+    function Avenger(nombre, equipo, nombreReal) {
+        this.nombre = "Sin Nombre";
+        this.puedePelear = false;
+        this.peleasGanadas = 0;
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nombreReal = nombreReal;
+    }
+    return Avenger;
+}());
+var antman = new Avenger("Antman", "cap", "Scott Lang");
+console.log(antman);
