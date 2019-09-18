4c4e38c1836a4e27e7c78bd9bbbf78a33b2512b4
alan-serrano <alan.serrano.pch@gmail.com>
2019-09-18 11:22:47 -0400

    Interfaz TypeScript

---
+++ Ejercicios/19.-Interfaces de TypeScript/app.js
@@ -0,0 +1,11 @@
+"use strict";
+function enviarMision(xmen) {
+    // Si trato de llamar a una variable del objeto xmen que no sea ni "nombre", ni "poder", TSC me mostraría un error
+    console.log(`Enviando a: ${xmen.nombre}`);
+}
+;
+let wolverine = {
+    nombreXmen: "Tormenta",
+    poder: "crear tormentas"
+};
+enviarMision(wolverine); //La función da error porque "wolverine" no es del tipo Xmen (Interfaz xmen)
---
+++ Ejercicios/19.-Interfaces de TypeScript/readme.md
@@ -0,0 +1,3 @@
+# *Types*, *Interfaces*
+
+Explicación de *Types* e *Interface* en el siguiente vínculo: <https://medium.com/@jesusmurfontanals/typescript-para-que-sirven-los-types-y-interfaces-y-en-que-se-diferencian-bee0af17fffa>
