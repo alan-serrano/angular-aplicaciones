c98c85b735ff7c434b4f7c5bcfacf86c3290c8d3
alan-serrano <alan.serrano.pch@gmail.com>
2019-09-21 14:44:13 -0400

    Ejercicios PIPES

---
+++ 00-Ejercicios/42.-Pipes/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'pipes';
+    nombre = 'Ricardo';
+    arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    PI = 3.14159265359;
+    proporcion = 0.45679;
+    precio = 150000.5;
+
+    heroe = {
+        nombre: 'Logan',
+        clave: 'Wolverine',
+        edad: 500,
+        direccion: {
+            calle: 'Primera',
+            casa: '19'
+        }
+    };
+    valorDePromesa = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('Llegó la data'), 3500);
+    });
+    fecha = new Date();
+}
+
